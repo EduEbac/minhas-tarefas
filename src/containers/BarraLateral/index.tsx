12b45a69d052ab8ab3a1bc3913d5ccff725370d4
EduEbac <edu.cursoebac@gmail.com>
2026-07-06 14:12:48 -0300

    M30_Projeto 5: Concluindo estilização básica na barra lateral

---
+++ src/containers/BarraLateral/index.tsx
@@ -0,0 +1,21 @@
+import FiltroCard from '../../components/FiltroCard'
+
+import * as S from './styles'
+
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Buscar" />
+    </div>
+    <S.Filtros>
+      <FiltroCard />
+      <FiltroCard />
+      <FiltroCard />
+      <FiltroCard />
+      <FiltroCard />
+      <FiltroCard ativo />
+    </S.Filtros>
+  </S.Aside>
+)
+
+export default BarraLateral
